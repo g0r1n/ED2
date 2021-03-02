6db4be9fa14621b8b0b21ee038182307e486e0d6
Wilson Goltara <wilson.goltara15@gmail.com>
2021-03-02 17:04:13 -0300

    finish ED2.c

--- ED2-v1.c
+++ ED2-v1.c
@@ -2,7 +2,7 @@
 /*****************************************
    Enunciado: Calculadora IRPF
    Autor: Wilson da Silva Goltara
-   Histórico de versões:
+   Histórico de versões: Version 1.0
 *****************************************/
 
 /* Importações de Bibliotecas */
@@ -19,41 +19,67 @@
 int main() {
     /* Declarações de variáveis */
    int num_filhos, faixa;
-   float sal_bruto, aliq, aliq_efe;
+   float sal_bruto, valor_pagar,aliq, aliq_efe, desc;
 
-    // ENTRADA
-   printf("=== CALCULADORA IRPF  ===/n");
-   printf("Digite o salário bruto(Válido somente números postivos):/n");
+    // ENTRADA                                                              /* Na parte da entrada eu fiz a aprensentação da calculadora e recolhi
+   printf("=== CALCULADORA IRPF  ===\n");                                   /*os dados armazenando em suas devidas variáveis.*/
+   printf("Digite o salário bruto(Válido somente números postivos):\n");
    scanf("%f", &sal_bruto);
-   printf("Digite o n° de dependentes(Válido somente números postivos):/n");
+   printf("Digite o n° de dependentes(Válido somente números postivos):\n");
    scanf("%d", &num_filhos);
 
-    // PROCESSAMENTO
-    if (sal_bruto>=2000){
-        printf("Você está insento do IPRF./n");
+    // PROCESSAMETO                                                         /* No primeiro momento, verifico se os número de filhos são maiores
+    if (num_filhos < 0 && sal_bruto < 0){                                   /* do que 0 e se o maxmimo de desconto foi atingido.*/
+        printf("números negativos não são possveis!!  ERRO!!");
+    }
+    desc = num_filhos*200;
+
+    if (desc > 1000){
+        desc = 1000;
+    }
+                                                                            /*No segundo momento verifo qual faixa o cliente se encaixa, armazenando e imprimindo no final*/
+    if (sal_bruto<=2000){                                                   /*o valor a ser pago, a faixa, a alíquota efetiva, dependentes, e o sálario bruto*/
+        printf("Você está insento do IPRF.\n");
     }else{
         if (sal_bruto>2000 && sal_bruto<=2900){
-            aliq = (sal_bruto*0.075) - (142,80 * num_filhos);
+            valor_pagar = (sal_bruto*0.075) - 142.80 - desc;
+            aliq_efe = (valor_pagar/sal_bruto)*100;
             faixa = 2;
+            aliq = 7.5;
         }else{
             if (sal_bruto>2900 && sal_bruto<=3800){
-                aliq = (sal_bruto*0.15) - (354,80*sal_bruto);
+                valor_pagar = (sal_bruto*0.075) - 142.80 - desc;
+                aliq_efe = (valor_pagar/sal_bruto)*100;
                 faixa = 3;
+                aliq = 15;
         }else{
             if (sal_bruto>3800 && sal_bruto<=4700){
-                aliq = (sal_bruto*0.225) - (636,13*sal_bruto);
+                valor_pagar = (sal_bruto*0.225) - 636.13 - desc;
+                aliq_efe = (valor_pagar/sal_bruto)*100;
                 faixa = 4;
+                aliq = 22.5;
         }else{
             if (sal_bruto>4700){
-                aliq = (sal_bruto*0.275) - (869,36*sal_bruto);
+                valor_pagar = (sal_bruto*0.275) - 869.36 - desc;
+                aliq_efe = (valor_pagar/sal_bruto)*100;
                 faixa = 5;
-                }}}}
+                aliq = 27.5;
+                }}}}}
 
-    printf("Seu salário bruto eh: %.2f reais", sal_bruto);
-    printf("Voce está na FAIXA %d", faixa);
-    printf("Voce possui %d dependentes", num_filhos);
-    printf("O valor de IRPF que deve ser pago eh: %.2f reias", aliq);
-    printf("Sendo assim, sua alíquota efetiva eh: %.2f %", aliq_efe);
-
+    if (valor_pagar > 0){                                               /*Aqui eu verifico se o valor a ser pago sera positivo e mostro as informações*/
+        printf("Seu salário bruto eh: %.2f reais\n", sal_bruto);        /*finais, com o salario bruto, numeros de filhos,valor a ser pago, */
+        printf("Voce está na FAIXA %d, com a alíquota de %d %n", &faixa, aliq);
+        printf("Voce possui %d dependentes\n", num_filhos);
+        printf("O valor de IRPF que deve ser pago eh: %.2f reias\n", valor_pagar);
+        printf("Sendo assim, sua alíquota efetiva eh: %.2f %\n", aliq_efe);
+    }else{
+        valor_pagar = 0;
+        aliq_efe = 0;
+        printf("Seu salário bruto eh: %.2f reais\n", sal_bruto);
+        printf("Voce está na FAIXA %d com a alíquota de %d %n", &faixa, aliq);
+        printf("Voce possui %d dependentes\n", num_filhos);
+        printf("O valor de IRPF que deve ser pago eh: %.2f reias\n", valor_pagar);
+        printf("Sendo assim, sua alíquota efetiva eh: %.2f %\n", aliq_efe);
+    }
     return 0;
 }

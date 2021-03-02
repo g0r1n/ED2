140311067fb9673295a4898781b8e1bac1b69ec5
Wilson Goltara <wilson.goltara15@gmail.com>
2021-03-02 15:21:20 -0300

    readed of code

--- ED2-v1.c
+++ ED2-v1.c
@@ -1,7 +1,7 @@
 
 /*****************************************
-   Enunciado: Calculadora IRPF  
-   Autor: Wilson da Silva Goltara 
+   Enunciado: Calculadora IRPF
+   Autor: Wilson da Silva Goltara
    Histórico de versões:
 *****************************************/
 
@@ -13,13 +13,13 @@
 /* Declaração de estruturas
    Nome: Calculadora IRPF
    Objetivo: Tem como objetivo informar ao usuário, a faixa e alíquota que o mesmo se encaixa, o valor a ser pago
-   e qual a alíquota efetiva. */ 
+   e qual a alíquota efetiva. */
 
 /* Funçõe principal */
 int main() {
     /* Declarações de variáveis */
-   int num_filhos;
-   float sal_bruto;
+   int num_filhos, faixa;
+   float sal_bruto, aliq, aliq_efe;
 
     // ENTRADA
    printf("=== CALCULADORA IRPF  ===/n");
@@ -27,7 +27,33 @@ int main() {
    scanf("%f", &sal_bruto);
    printf("Digite o n° de dependentes(Válido somente números postivos):/n");
    scanf("%d", &num_filhos);
+
     // PROCESSAMENTO
-    
+    if (sal_bruto>=2000){
+        printf("Você está insento do IPRF./n");
+    }else{
+        if (sal_bruto>2000 && sal_bruto<=2900){
+            aliq = (sal_bruto*0.075) - (142,80 * num_filhos);
+            faixa = 2;
+        }else{
+            if (sal_bruto>2900 && sal_bruto<=3800){
+                aliq = (sal_bruto*0.15) - (354,80*sal_bruto);
+                faixa = 3;
+        }else{
+            if (sal_bruto>3800 && sal_bruto<=4700){
+                aliq = (sal_bruto*0.225) - (636,13*sal_bruto);
+                faixa = 4;
+        }else{
+            if (sal_bruto>4700){
+                aliq = (sal_bruto*0.275) - (869,36*sal_bruto);
+                faixa = 5;
+                }}}}
+
+    printf("Seu salário bruto eh: %.2f reais", sal_bruto);
+    printf("Voce está na FAIXA %d", faixa);
+    printf("Voce possui %d dependentes", num_filhos);
+    printf("O valor de IRPF que deve ser pago eh: %.2f reias", aliq);
+    printf("Sendo assim, sua alíquota efetiva eh: %.2f %", aliq_efe);
+
     return 0;
 }
